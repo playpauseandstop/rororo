b1493728d4f1c9bdf1f42726dc4e085d87c4f55d
Igor Davydenko <playpauseandstop@gmail.com>
2012-10-22 19:13:04 +0300

    Refactor all views from application module to ``app`` and ``views`` modules, leave only ``application`` and ``process_response`` functions, all routes declared now in ``views`` module.

--- app.py
+++ app.py
@@ -1,64 +1,35 @@
-import json
 import os
-import uuid
 
 from jinja2 import Environment, PackageLoader
-from routr import GET, POST, route
-from routr.utils import inject_args
+from routr import include, route
 from routr.exc import NoMatchFound
-from routrschema import qs
-from schemify import opt
+from routr.utils import inject_args
 from webob.exc import HTTPException
 from webob.request import Request
 from webob.response import Response
 from wsgiref.simple_server import make_server
 
+from views import routes
 
-def environ():
-    return dict(os.environ)
 
-def hello():
-    return 'Hello, world!'
-
-def page(pk):
-    return 'Page #{}!'.format(int(pk))
-
-def search(query=None):
-    if not query:
-        return 'Enter query to search'
-    return 'Searching {}...'.format(query)
-
-def template_view():
-    return {'var': uuid.uuid4()}
-
-def show_request(request, path):
-    return {
-        'method': request.method,
-        'path': path,
-        'headers': dict(request.headers),
-    }
-
-routes = route(
-    GET('/', hello, name='hello'),
-    GET('/environ', environ, name='environ', renderer='json'),
-    GET('/page/{pk:int}', page, name='page'),
-    GET('/search', qs(query=opt(str)), search, name='search'),
-    GET('/template', template_view, name='template', renderer='template.html'),
-    GET('/request/{path:path}', show_request, renderer='json'),
+routes = route('',
+    include('views:routes'),
 )
-
-env = Environment(loader=PackageLoader('app', 'templates'))
-env.globals['reverse'] = routes.reverse
 
 
 def application(environ, start_response):
+    """
+    Main WSGI application.
+    """
     request = Request(environ)
 
     try:
         trace = routes(request)
         view = trace.target
+
         args, kwargs = trace.args, trace.kwargs
         args = inject_args(view, args, request=request)
+
         response = view(*args, **kwargs)
         if not isinstance(response, Response):
             response = process_renderer(trace, response)
@@ -66,21 +37,28 @@ def application(environ, start_response):
         response = e.response
     except HTTPException, e:
         response = e
+
     return response(environ, start_response)
 
+
 def process_renderer(trace, data):
-    """ Extract renderer metadata from ``trace`` and apply specified renderer to
-    ``data``"""
+    """
+    Extract renderer metadata from ``trace`` and apply specified renderer to
+    ``data``.
+    """
     renderer = trace.endpoint.annotations.get('renderer')
+
     if renderer is None:
         return Response(data)
     elif renderer == 'json':
         return Response(json=data)
     elif renderer.endswith('.html'):
+        env = Environment(loader=PackageLoader('app', 'templates'))
+        env.globals['reverse'] = routes.reverse
         template = env.get_template(renderer)
         return Response(template.render(**data))
-    else:
-        raise ValueError('unknown renderer %s' % renderer)
+
+    raise ValueError('Unknown renderer {0!r}'.format(renderer))
 
 if __name__ == '__main__':
     server = make_server(
---
+++ views.py
@@ -0,0 +1,49 @@
+import os
+import uuid
+
+from routr import GET, route
+from routrschema import qs
+from schemify import opt
+
+
+def environ():
+    return dict(os.environ)
+
+
+def hello():
+    return 'Hello, world!'
+
+
+def page(pk):
+    return 'Page #{0:d}'.format(int(pk))
+
+
+def search(query=None):
+    if not query:
+        return 'Enter query to search'
+    return 'Searching {}...'.format(query)
+
+
+def show_request(request, path):
+    return {
+        'headers': dict(request.headers),
+        'method': request.method,
+        'path': path,
+    }
+
+
+def template():
+    return {'var': uuid.uuid4()}
+
+
+routes = route('',
+    GET('/', hello, name='hello'),
+    GET('/environ', environ, name='environ', renderer='json'),
+    GET('/page/{pk:int}', page, name='page'),
+    GET('/search', qs(query=opt(str)), search, name='search'),
+    GET('/request/{path:path}',
+        show_request,
+        name='show_request',
+        renderer='json'),
+    GET('/template', template, name='template', renderer='template.html'),
+)
