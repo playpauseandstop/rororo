feac1c1a7dff986a9dce609aacdca159ff89471f
Igor Davydenko <playpauseandstop@gmail.com>
2013-04-29 16:21:52 +0300

    Argparse required for Python 2.6.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup
 
 
+IS_PY26 = sys.version_info[:2] == (2, 6)
 README = open('README.rst').read()
 VERSION = '0.1'
 
@@ -18,14 +21,15 @@ setup(
     packages=[
         'rororo',
     ],
-    install_requires=[
+    install_requires=list(filter(None, [
         'Jinja2>=2.6',
         'WebOb>=1.2.3',
+        'argparse==1.2.1' if IS_PY26 else None,
         'routr>=0.7.0',
         'routr-schema>=0.1',
         'schemify>=0.1',
         'server-reloader>=0.1.2',
-    ],
+    ])),
     platforms='any',
     classifiers=[
         'Development Status :: 3 - Alpha',
