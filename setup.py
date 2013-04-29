05e340acb930ff71116117887c431ded1f0c6677
Igor Davydenko <playpauseandstop@gmail.com>
2013-04-29 16:08:31 +0300

    Modify classifiers in setup script.

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ VERSION = '0.1'
 setup(
     name='rororo',
     version=VERSION,
-    description='Functional nano web-framework built on top of WebOb, routr ' \
-                'and Jinja',
+    description=('Functional nano web-framework built on top of WebOb, routr '
+                 'and Jinja'),
     long_description=README,
     author='Igor Davydenko',
     author_email='playpauseandstop@gmail.com',
@@ -21,9 +21,9 @@ setup(
     install_requires=[
         'Jinja2>=2.6',
         'WebOb>=1.2.3',
-        'routr==0.6.2',
-        'routr-schema==0.1',
-        'schemify==0.1',
+        'routr>=0.7.0',
+        'routr-schema>=0.1',
+        'schemify>=0.1',
         'server-reloader>=0.1.2',
     ],
     platforms='any',
@@ -31,9 +31,11 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
-        'Topic :: Utilities',
         'Programming Language :: Python',
+        'Topic :: Utilities',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'License :: OSI Approved :: BSD License',
     ],
     keywords='nano web framework webob routr jinja',
