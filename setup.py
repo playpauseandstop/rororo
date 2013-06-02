9184cf6d31ae7d4da512d0e241b9dd296dae7212
Igor Davydenko <playpauseandstop@gmail.com>
2013-06-03 02:18:55 +0300

    Updates to setup script, read version from init file, pin Jinja2 requirement.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+import os
+import re
 import sys
 
 from setuptools import setup
 
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+rel = lambda *parts: os.path.abspath(os.path.join(DIRNAME, *parts))
 
-IS_PY26 = sys.version_info[:2] == (2, 6)
-README = open('README.rst').read()
-VERSION = '0.1'
+README = open(rel('README.rst')).read()
+INIT_PY = open(rel('rororo', '__init__.py')).read()
+VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
+
 
 setup(
     name='rororo',
@@ -22,14 +27,14 @@ setup(
         'rororo',
     ],
     install_requires=list(filter(None, [
-        'Jinja2>=2.6',
+        'Jinja2==2.6',
         'WebOb>=1.2.3',
-        'argparse==1.2.1' if IS_PY26 else None,
+        'argparse==1.2.1' if sys.version_info[:2] < (2, 7) else None,
         'routr>=0.7.1',
         'routr-schema>=0.1',
         'schemify>=0.1',
         'server-reloader>=0.1.2',
-        'unittest2==0.5.1' if IS_PY26 else None,
+        'unittest2==0.5.1' if sys.version_info[:2] < (2, 7) else None,
     ])),
     platforms='any',
     classifiers=[
@@ -37,6 +42,11 @@ setup(
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
