7c9cf6995a11df67363b7d30e78c28140e8bc30c
Igor Davydenko <playpauseandstop@gmail.com>
2013-11-12 13:41:33 +0200

    Use normal imports when importing necessary shared parts in routes and schema modules.

--- rororo/routes.py
+++ rororo/routes.py
@@ -7,11 +7,6 @@ Provide shortcuts for routes functions and helpers.
 
 """
 
-from .utils import inject_module
-
-
-inject_module('routr',
-              locals(),
-              include_attr=('DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH',
-                            'POST', 'PUT', 'TRACE', 'include', 'plug',
-                            'route'))
+from routr import (  # noqa
+    DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE, include, plug, route
+)
--- rororo/schema.py
+++ rororo/schema.py
@@ -9,7 +9,8 @@ Contains all custom and routr's schemas (guards) and validators.
 
 import copy
 
-from .utils import inject_module
+from routrschema import RequestParams, form, json_body, qs  # noqa
+from schemify import anything, opt, validate  # noqa
 
 
 class defaults(object):
@@ -27,14 +28,3 @@ class defaults(object):
         Update trace kwargs with defaults values.
         """
         trace.kwargs.update(copy.deepcopy(self.__dict__))
-
-
-# Include schemas from routrschema module
-inject_module('routrschema',
-              locals(),
-              include_attr=('RequestParams', 'form', 'json_body', 'qs'))
-
-# Include validators from schemify module
-inject_module('schemify',
-              locals(),
-              include_attr=('anything', 'opt', 'validate'))
