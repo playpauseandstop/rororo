28a04e8028c29647941e727116335e9d6fd64c27
Igor Davydenko <iam@igordavydenko.com>
2019-03-24 14:59:20 +0200

    chore: Regenerate `setup.py` file.

--- setup.py
+++ setup.py
@@ -8,26 +8,7 @@ package_data = \
 {'': ['*']}
 
 install_requires = \
-['Sphinx[docs]>=1.8,<2.0',
- 'aiohttp[tests]>=3.5,<4.0',
- 'coverage[tests]>=4.5,<5.0',
- 'coveralls[tests]>=1.7,<2.0',
- 'fastjsonschema[tests]>=2.9,<3.0',
- 'flake8-bugbear[lint]>=18.8,<19.0',
- 'flake8-commas[lint]>=2.0,<3.0',
- 'flake8-comprehensions[lint]>=2.1,<3.0',
- 'flake8-docstrings[lint]>=1.3,<2.0',
- 'flake8-import-order[lint]>=0.18.1,<0.19.0',
- 'flake8-mutable[lint]>=1.2,<2.0',
- 'flake8-quotes[lint]>=1.0,<2.0',
- 'flake8-string-format[lint]>=0.2.3,<0.3.0',
- 'flake8-tidy-imports[lint]>=2.0,<3.0',
- 'flake8[lint]>=3.7,<4.0',
- 'jsl[tests]>=0.2.4,<0.3.0',
- 'jsonschema>=2.6,<3.0',
- 'mypy[lint]>=0.670.0,<0.671.0',
- 'pep8-naming[lint]>=0.8.2,<0.9.0',
- 'sphinx_autodoc_typehints[docs]>=1.6,<2.0']
+['jsonschema>=2.6,<3.0']
 
 setup_kwargs = {
     'name': 'rororo',
