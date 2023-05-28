bc8c9f034b5258099b6fc8a0869710bed3ddb79d
Igor Davydenko <iam@igordavydenko.com>
2023-05-28 15:00:22 +0200

    feat: Ensure support of `environ-config@^23.2.0` (#360)
    As `environ-config@23.1.0` ships not totally correct type annotations
    it is decided to **bump** minimum supported environ-config version to
    23.2.0 and update `rororo` code to reflect library type hints.

--- examples/petstore/src/petstore/settings.py
+++ examples/petstore/src/petstore/settings.py
@@ -3,6 +3,6 @@ import environ
 from rororo import BaseSettings
 
 
-@environ.config(prefix=None, frozen=True)
+@environ.config(prefix="", frozen=True)
 class Settings(BaseSettings):
     pets_app_key: str = environ.var(name="PETS_APP_KEY", default="pets")
